6631cd40641e90a11e9858ed51fa882075ed197c
FatPig1824 <141415117+FatPig1824@users.noreply.github.com>
2024-03-13 00:31:59 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -1,21 +1,33 @@
+import javax.swing.*;
+
 public class Conta {
     public int numero;
     public double saldo;
+    public int debitoSaldo;
 
+    public int creditoSaldo;
     @Override
     public String toString() {
-        return "Numero: "+numero+"\nSaldo: "+saldo;
+        return "Numero: " + numero + "\nSaldo: " + saldo;
     }
-    public double debitoSaldo( double dSaldo){
-        dSaldo = saldo;
-        saldo = saldo + dSaldo;
-        return saldo;
-    }
-    public double creditoSaldo(double cSaldo){
-        cSaldo = saldo;
-       saldo = saldo - cSaldo;
-       return saldo;
 
-    }}
+    public double debitoSaldo(double saldo) {
+        if(saldo >=0){
+       saldo += debitoSaldo;
+
+        }
+        else{
+            JOptionPane.showMessageDialog(null,"Você não tem saldo suficiente","Informação",JOptionPane.ERROR_MESSAGE);
+        }
+
+return saldo;
+  }  public double creditoSaldo(double saldo) {
+        saldo -= creditoSaldo;
+
+
+
+return saldo;
+    }
+}
 
 
